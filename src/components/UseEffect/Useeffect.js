3f0b7f0041f3f9b59daec97d61bdc3b6100b5128
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-10-07 21:03:57 +0600

    useEffect and you cant use the setInterval in Jsx

--- src/components/UseEffect/Useeffect.js
+++ src/components/UseEffect/Useeffect.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Useeffect = () => {
-    const [time,setTime] = React.useState(new Date().toLocaleTimeString());
+    const [times,setTime] = React.useState(new Date().toLocaleTimeString());
 
     React.useEffect(() =>{
         setInterval(() =>setTime(new Date().toLocaleTimeString()),1000);
     })
     return (
         <div>
-            <h1>{time}</h1>
+            <h1>{times}</h1>
             <button onClick={()=>setTime(new Date().toLocaleTimeString())}>newTime</button>
         </div>
     );
